e986bcecd6b4434581d5cfa903ea506d305b84ba
Phoenix <153451582+3xpl017@users.noreply.github.com>
2024-01-14 09:30:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
+clear
 echo --------------------------------------------------------------------------------------------
 echo [>] Instalando requerimientos...
 echo --------------------------------------------------------------------------------------------
 sudo su
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
 sudo apt install msfvenom
-pip3 install 
+pip3 install os
+pip3 install colarama
+pip3 install sys
+pip3 install time
+pip3 install socket
+echo [>] Requerimientos instalados. Usa el comando python3 main.py para inciar Scorpion.
